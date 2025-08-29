8eb3b7bf05ae34d2a0b11be68ba016abaf09348a
Dale <Danntabachoyy@gmail.com>
2025-08-29 20:25:26 +0800

    implemented views part 2: now views count update whenever client visit the startup. thou a lil bit buggy, gonna try to fix it tom :)

--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,24 @@
 import { client } from '@/sanity/lib/client';
 import Ping from './Ping';
 import { STARTUP_VIEWS_QUERY_BY_ID } from '@/sanity/lib/queries';
+import { writeClient } from '@/sanity/lib/write-client';
+import { after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
-  const { views }: { views: number } = await client.fetch(
-    STARTUP_VIEWS_QUERY_BY_ID,
-    { id }
-  );
+  const { views: totalViews }: { views: number } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY_BY_ID, { id });
+
+  // await writeClient
+  //   .patch(id)
+  //   .set({ views: totalViews + 1 })
+  //   .commit();
+  // after(async () => {
+  //   await writeClient
+  //     .patch(id)
+  //     .set({ views: totalViews + 1 })
+  //     .commit();
+  // });
 
   return (
     <div className='view-container'>
@@ -16,7 +28,7 @@ const View = async ({ id }: { id: string }) => {
 
       <p className='view-text'>
         <span className='font-black'>
-          {views > 1 ? 'Views' : 'View'}: {views}
+          {totalViews > 1 ? 'Views' : 'View'}: {totalViews}
         </span>
       </p>
     </div>
